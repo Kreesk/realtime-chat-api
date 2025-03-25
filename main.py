a5d6537062fd7e98579eae2c9ef49dcbc225952b
Kreesk <dwql67@gmail.com>
2025-03-25 21:18:35 +0300

    Add username support to WebSocket chat

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from typing import List
+from typing import Dict
 
 app = FastAPI(title='Real-time Chat API')
 
-connections: List[WebSocket] = []
+connections: Dict[WebSocket, str] = {}
 
 @app.websocket("/chat/")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    connections.append(websocket)
+
+    await websocket.send_text("Пожалуйста введите свое имя:")
+    username = await websocket.receive_text()
+
+    connections[websocket] = username
+
     try:
+        for connection in connections:
+            await connection.send_text(f'{username} вошел в чат!')
+
         while True:
             data = await websocket.receive_text()
             for connection in connections:
-                await connection.send_text(f'Message: {data}')
+                await connection.send_text(f'{username}: {data}')
     except WebSocketDisconnect:
-        connections.remove(websocket)
+        connections.pop(websocket)
+
+        for connection in connections:
+            await connection.send_text(f"{username} вышел из чата.")
